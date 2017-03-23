8d0a21677bc78c6a77ea94fc7a6b06c9c4a7aabf
LeiSurrre <ben.yutong.chen@hotmail.com>
2017-03-22 20:14:22 -0400

    add player nav
    debugged power up, add player nav

--- ben/c09project/js/script.js
+++ ben/c09project/js/script.js
@@ -13,9 +13,11 @@
     }
     
     var canvas=document.getElementById("game_board");
+    canvas.setAttribute('width', '800px');
+    canvas.setAttribute('height', '500px');
     var ctx = canvas.getContext("2d");
-    var p1 = new Player({'username':"p1", 'id':"p1", 'x':100, 'y':100, 'role':"run"});
-    var p2 = new Player({'username':"p2", 'id':"p2", 'x':700, 'y':400, 'role':"catch"});
+    var p1 = new Player({'username':"p1", 'id':"p1", 'x':10, 'y':10, 'role':"run"});
+    var p2 = new Player({'username':"p2", 'id':"p2", 'x':720, 'y':460, 'role':"catch"});
     p2.dx = 5;
     p2.dy = 5;
     var playerWidth = 30;
@@ -23,186 +25,85 @@
     var powerX, powerY;
     var itemWidth = 10;
 
-    // hit wall
-    /*var ballRadius = 10;
-    var x = canvas.width/2;
-    var y = canvas.height-30;
-    var dx = 0.5;
-    var dy = -0.5;
-    var dx2 = 0.8;
-    var dy2 = -0.8;
-    var x2 = 100;
-    var y2 = 100;
-    var hit = 0;
-    var hit2 = 0;
-    var color1 = "#"+((1<<24)*Math.random()|0).toString(16);
-    var color2 = "#"+((1<<24)*Math.random()|0).toString(16);
-
-    function drawBall() {
-        ctx.beginPath();
-        ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-        if (hit == 1) {
-            color1 = "#"+((1<<24)*Math.random()|0).toString(16);
-            ctx.fillStyle = color1;
-            hit = 0;
-        } else {
-            ctx.fillStyle = color1;
-        }
-        ctx.fill();
-        ctx.closePath();
-    }
-
-    function drawBall2() {
-        ctx.beginPath();
-        ctx.arc(x2, y2, ballRadius, 0, Math.PI*2);
-        if (hit2 == 1) {
-            color2 = "#"+((1<<24)*Math.random()|0).toString(16);
-            ctx.fillStyle = color2;
-            hit2 = 0;
-        } else {
-            ctx.fillStyle = color2;
-        }
-        ctx.fill();
-        ctx.closePath();
-    }
-
-
-    function draw() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawBall();
-        drawBall2();
-        
-        if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-            dx = -dx;
-            hit = 1;
-        } else if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-            dy = -dy;
-            hit = 1;
-        } else if(x2 + dx2 > canvas.width-ballRadius || x2 + dx2 < ballRadius) {
-            dx2 = -dx2;
-            hit2 = 1;
-        } else if(y2 + dy2 > canvas.height-ballRadius || y2 + dy2 < ballRadius) {
-            dy2 = -dy2;
-            hit2 = 1;
-        }
-        
-        x += dx;
-        y += dy;
-        x2 +=dx2;
-        y2 += dy2;
-    }
-
-    setInterval(draw, 10);*/
-
-    // follow cursor
-    /*var targetX = 0,
-    targetY = 0,
-    x = 10,
-    y = 10,
-    velX = 0,
-    velY = 0,
-    speed = 4;
-
-    function update(){
-        var tx = targetX - x,
-            ty = targetY - y,
-            dist = Math.sqrt(tx*tx+ty*ty),
-            rad = Math.atan2(ty,tx),
-            angle = rad/Math.PI * 180;
-
-        velX = (tx/dist)*speed;
-        velY = (ty/dist)*speed;
-        
-        if (targetX > x + velX && velX >= 0) {
-            x += velX;
-        } else if (targetX < x + velX && velX >= 0) {
-            x = targetX;
-        } else if (targetX < x + velX && velX < 0) {
-            x += velX;
-        } else if (targetX > x + velX && velX < 0) {
-            x = targetX;
-        }
-
-        if (targetY > y + velY) {
-            y += velY;
-        } else if (targetY < y + velY && velY >= 0) {
-            y = targetY;
-        } else if (targetY < y + velY && velY < 0) {
-            y += velY;
-        } else if (targetY > y + velY && velY < 0) {
-            y = targetY;
-        }
-
-        ctx.clearRect(0,0,800,500);
-        ctx.beginPath();
-        ctx.arc(x,y,20,0,Math.PI*2);
-        ctx.fillStyle = "#0095DD";
-        ctx.fill();
-        ctx.closePath();
-
-        setTimeout(update,10);
-    }
-
-    update();
-
-    canvas.addEventListener("mousemove", function(e){
-        targetX = e.clientX - canvas.offsetLeft;
-        targetY = e.clientY - canvas.offsetTop;
-    });*/
-
-    //  pick color
-    /*var mouseX, mouseY;
-    var grd = ctx.createLinearGradient(0,0,200,0);
-    var mycolor;
-    grd.addColorStop(0,"red");
-    grd.addColorStop(1,"white");
-
-    // Fill with gradient
-    ctx.fillStyle = grd;
-    ctx.fillRect(10,10,150,80);
-
-    ctx.fillStyle = "#0095DD";
-    ctx.fillRect(90,190,150,80);
-
-    ctx.fillStyle = "#000000";
-    ctx.fillRect(100,70,150,80);
-
-    ctx.fillStyle = "#"+((1<<24)*Math.random()|0).toString(16);
-    ctx.fillRect(560,70,150,80);
-
-    ctx.fillStyle = "#"+((1<<24)*Math.random()|0).toString(16);
-    ctx.fillRect(420,300,150,80);
-
-
-    function update(){
-        console.log(mouseX, mouseY);
-        mycolor = ctx.getImageData(mouseX,mouseY,1,1);
-        console.log("color", mycolor);
-
-        ctx.clearRect(500,500,800,500);
-        ctx.beginPath();
-        ctx.arc(700,400,20,0,Math.PI*2);
-        ctx.fillStyle = mycolor;
-        ctx.fill();
-
-    }
-
-    setInterval(update, 1000);
-
-    canvas.addEventListener("mousemove", function(e){
-    mouseX = e.clientX - canvas.offsetLeft;
-    mouseY = e.clientY - canvas.offsetTop;
-    });*/
-
+    var mapColor = "#dcdcdc";
 
     // keyboard move ball
-    var color1 = "#"+((1<<24)*Math.random()|0).toString(16);
-    var color2 = "#"+((1<<24)*Math.random()|0).toString(16);
+    var color1 = "red";
+    var color2 = "orange";
     var p1Left = false, p1Right = false, p1Up = false, p1Down = false;
     var p2Left = false, p2Right = false, p2Up = false, p2Down = false;
 
+    function fillColorBit(c) {
+        while (c.length < 6) {
+            c = "0" + c;
+        }
+        return c;
+    }
+
+    function parseColor(color) {
+        var s1 = ((color.data[0]<<16)|0);
+        var s2 = ((color.data[1]<<8)|0);
+        var s3 = ((color.data[2])|0);
+        var s4 = ((s1 + s2 + s3)|0).toString(16);
+        s4 = fillColorBit(s4);
+        return "#" + s4;
+    }
+
     function detectCollision(x1,y1,x2,y2,width,height) {
-        return (x1 > x2 && x1 < x2 + width && y1 > y2 && y1 < y2 + width);
+        return (x1 >= x2 && x1 <= x2 + width && y1 >= y2 && y1 <= y2 + width);
+    }
+
+    function detectWall(x,y,width,height) {
+        var c1 = parseColor(ctx.getImageData(x,y,1,1));
+        var c2 = parseColor(ctx.getImageData(x + width,y,1,1));
+        var c3 = parseColor(ctx.getImageData(x,y + height,1,1));
+        var c4 = parseColor(ctx.getImageData(x + width,y + height,1,1));
+        console.log(c1,c2,c3,c4);
+        console.log(c1 == mapColor || c2 == mapColor || c3 == mapColor || c4 == mapColor);
+        return (c1 == mapColor || c2 == mapColor || c3 == mapColor || c4 == mapColor);
+
+    }
+
+    function wallDistance(dir, p) {
+        var i;
+        var dy = p.dy;
+        var dx = p.dx;
+        if (dir == "up") {
+            for (i = 1; i <= dy; i++) {
+                var c1 = parseColor(ctx.getImageData(p.x,p.y - i,1,1));
+                var c2 = parseColor(ctx.getImageData(p.x + playerWidth,p.y - i,1,1));
+                console.log(c1, c2);
+                if (c1 == mapColor || c2 == mapColor) {
+                    return i;
+                }
+            }
+        } else if (dir == "down") {
+            for (i = 1; i <= dy; i++) {
+                var c1 = parseColor(ctx.getImageData(p.x,p.y + playerWidth + i,1,1));
+                var c2 = parseColor(ctx.getImageData(p.x + playerWidth,p.y + playerWidth + i,1,1));
+                if (c1 == mapColor || c2 == mapColor) {
+                    return i;
+                }
+            }
+        } else if (dir == "left") {
+            for (i = 1; i <= dx; i++) {
+                var c1 = parseColor(ctx.getImageData(p.x - i,p.y,1,1));
+                var c2 = parseColor(ctx.getImageData(p.x - i,p.y + playerWidth,1,1));
+                if (c1 == mapColor || c2 == mapColor) {
+                    return i;
+                }
+            }
+        } else if (dir == "right") {
+            for (i = 1; i <= dx; i++) {
+                var c1 = parseColor(ctx.getImageData(p.x + playerWidth + i,p.y,1,1));
+                var c2 = parseColor(ctx.getImageData(p.x + playerWidth + i,p.y + playerWidth,1,1));
+                if (c1 == mapColor || c2 == mapColor) {
+                    return i;
+                }  
+            }
+        }
+        // wall not found
+        return -1;
     }
 
     function drawPlayer(p) {
@@ -215,8 +116,27 @@
     }
 
     function drawItem() {
-        ctx.fillStyle = "lime";
+        ctx.fillStyle = "blue";
         ctx.fillRect(powerX, powerY, itemWidth, itemWidth);
+    }
+
+    function drawMap() {
+        ctx.fillStyle = mapColor;
+        ctx.fillRect(60,60,100,100);
+        ctx.fillRect(60,240,100,100);
+        ctx.fillRect(60,420,100,100);
+        ctx.fillRect(240,0,100,100);
+        ctx.fillRect(240,180,100,100);
+        ctx.fillRect(240,360,100,100);
+        ctx.fillRect(420,60,100,100);
+        ctx.fillRect(420,240,100,100);
+        ctx.fillRect(420,420,100,100);
+        ctx.fillRect(600,0,100,100);
+        ctx.fillRect(600,180,100,100);
+        ctx.fillRect(600,360,100,100);
+        ctx.fillRect(780,60,100,100);
+        ctx.fillRect(780,240,100,100);
+        ctx.fillRect(780,420,100,100);
     }
 
     function switchRole() {
@@ -246,14 +166,16 @@
         while (!valid) {
             powerX = parseInt((canvas.width - itemWidth)*Math.random());
             powerY = parseInt((canvas.height - itemWidth)*Math.random());
-            if (!detectCollision(powerX, powerY, p1.x, p1.y, playerWidth, playerWidth) &&
+            if ((powerX > 2 && powerY > 2 && powerX < canvas.width - 2 && powerY < canvas.height - 2) &&
+                !detectCollision(powerX, powerY, p1.x, p1.y, playerWidth, playerWidth) &&
                 !detectCollision(powerX + itemWidth, powerY, p1.x, p1.y, playerWidth, playerWidth) &&
                 !detectCollision(powerX, powerY + itemWidth, p1.x, p1.y, playerWidth, playerWidth) &&
                 !detectCollision(powerX + itemWidth, powerY + itemWidth, p1.x, p1.y, playerWidth, playerWidth) &&
                 !detectCollision(powerX, powerY, p2.x, p2.y, playerWidth, playerWidth) &&
                 !detectCollision(powerX + itemWidth, powerY, p2.x, p2.y, playerWidth, playerWidth) &&
                 !detectCollision(powerX, powerY + itemWidth, p2.x, p2.y, playerWidth, playerWidth) &&
-                !detectCollision(powerX + itemWidth, powerY + itemWidth, p2.x, p2.y, playerWidth, playerWidth)) {
+                !detectCollision(powerX + itemWidth, powerY + itemWidth, p2.x, p2.y, playerWidth, playerWidth) &&
+                !detectWall(powerX - 2, powerY - 2, itemWidth + 2, itemWidth + 2)) {
                 valid = true;
             }
         }
@@ -283,62 +205,172 @@
         if (cover) {
             if (p1.role == "catch") {
                 console.log("p1 win");
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.font = "80px Arial";
+                ctx.fillText("P1 WIN!!!",200,100);
             } else {
                 console.log("p2 win");
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.font = "80px Arial";
+                ctx.fillStyle = "black";
+                ctx.fillText("P2 WIN!!!",200,100);
             }
         }
         return cover;
     }
 
     function movePlayers() {
-        /* Up arrow was pressed */
+        if (p1Up) {
+            var d = wallDistance("up",p1);
+            console.log(d);
+            if (d > 0) {
+                d -= 1;
+                p1.y -= d;
+            } else {
+                if (p1.y - p1.dy > 0) {
+                    p1.y -= p1.dy;
+                } else {
+                    p1.y = 1;
+                }
+            }
+        } else if (p1Down) {
+            var d = wallDistance("down",p1);
+            if (d > 0) {
+                d -= 1;
+                p1.y += d;
+            } else {
+                if (p1.y + p1.dy + playerWidth < canvas.height) {
+                    p1.y += p1.dy;
+                } else {
+                    p1.y = canvas.height - playerWidth - 1;
+                }
+            }
+        } else if (p1Left) {
+            var d = wallDistance("left",p1);
+            if (d > 0) {
+                d -= 1;
+                p1.x -= d;
+            } else {
+                if (p1.x - p1.dx > 0) {
+                    p1.x -= p1.dx;
+                } else {
+                    p1.x = 1;
+                }
+            }
+        } else if (p1Right) {
+            var d = wallDistance("right",p1);
+            if (d > 0) {
+                d -= 1;
+                p1.x += d;
+            } else {
+                if (p1.x + p1.dx + playerWidth < canvas.width) {
+                    p1.x += p1.dx;
+                } else {
+                    p1.x = canvas.width - playerWidth - 1;
+                }
+            }
+        }
+/*
+        // Up arrow was pressed
         if (p1Up && p1.y - p1.dy > 0) {
             p1.y -= p1.dy;
         } else if (p1Up && p1.y - p1.dy <= 0) {
             p1.y = 0;
         }
-        /* Down arrow was pressed */
+        // Down arrow was pressed
         else if (p1Down && p1.y + p1.dy + playerWidth < canvas.height) {
             p1.y += p1.dy;
         } else if (p1Down && p1.y + p1.dy + playerWidth >= canvas.height) {
             p1.y = canvas.height - playerWidth;
         }
-        /* Left arrow was pressed */
+        // Left arrow was pressed
         else if (p1Left && p1.x - p1.dx > 0) {
             p1.x -= p1.dx;
         } else if (p1Left && p1.x - p1.dx <= 0) {
             p1.x = 0;
         }
-        /* Right arrow was pressed */
+        // Right arrow was pressed
         else if (p1Right && p1.x + p1.dx + playerWidth < canvas.width){
             p1.x += p1.dx;
         } else if (p1Right && p1.x + p1.dx + playerWidth >= canvas.width) {
             p1.x = canvas.width - playerWidth;
         }
 
-        /* Up arrow(w) was pressed */
+        // Up arrow(w) was pressed
         if (p2Up && p2.y - p2.dy > 0){
             p2.y -= p2.dy;
         } else if (p2Up && p2.y - p2.dy <= 0) {
             p2.y = 0;
         }
-        /* Down arrow(s) was pressed */
+        // Down arrow(s) was pressed
         else if (p2Down && p2.y + p2.dy + playerWidth < canvas.height){
             p2.y += p2.dy;
         } else if (p2Down && p2.y + p2.dy + playerWidth >= canvas.height) {
             p2.y = canvas.height - playerWidth;
         }
-        /* Left arrow(a) was pressed */
+        // Left arrow(a) was pressed 
         else if (p2Left && p2.x - p2.dx > 0){
             p2.x -= p2.dx;
         } else if (p2Left && p2.x - p2.dx <= 0) {
             p2.x = 0;
         }
-        /* Right arrow(d) was pressed */
+        // Right arrow(d) was pressed
         else if (p2Right && p2.x + p2.dx + playerWidth < canvas.width){
             p2.x += p2.dx;
         } else if (p2Right && p2.x + p2.dx + playerWidth >= canvas.width) {
             p2.x = canvas.width - playerWidth;
+        }*/
+
+        if (p2Up) {
+            var d = wallDistance("up",p2);
+            console.log(d);
+            if (d > 0) {
+                d -= 1;
+                p2.y -= d;
+            } else {
+                if (p2.y - p2.dy > 0) {
+                    p2.y -= p2.dy;
+                } else {
+                    p2.y = 1;
+                }
+            }
+        } else if (p2Down) {
+            var d = wallDistance("down",p2);
+            console.log(d);
+            if (d > 0) {
+                d -= 1;
+                p2.y += d;
+            } else {
+                if (p2.y + p2.dy + playerWidth < canvas.height) {
+                    p2.y += p2.dy;
+                } else {
+                    p2.y = canvas.height - playerWidth - 1;
+                }
+            }
+        } else if (p2Left) {
+            var d = wallDistance("left",p2);
+            if (d > 0) {
+                d -= 1;
+                p2.x -= d;
+            } else {
+                if (p2.x - p2.dx > 0) {
+                    p2.x -= p2.dx;
+                } else {
+                    p2.x = 1;
+                }
+            }
+        } else if (p2Right) {
+            var d = wallDistance("right",p2);
+            if (d > 0) {
+                d -= 1;
+                p2.x += d;
+            } else {
+                if (p2.x + p2.dx + playerWidth < canvas.width) {
+                    p2.x += p2.dx;
+                } else {
+                    p2.x = canvas.width - playerWidth - 1;
+                }
+            }
         }
     }
 
@@ -408,9 +440,9 @@
 
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawMap();
         movePlayers();
         if (detectCatch()){
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
             return;
         }
         detectPowerUp(p1);
@@ -421,11 +453,11 @@
         requestAnimationFrame(draw);
     }
 
+    drawMap();
     makePowerUp();
     draw();
 
     document.addEventListener("keydown", keyDownHandler, true);
     document.addEventListener("keyup", keyUpHandler, false);
-    //document.addEventListener('keydown',doP2KeyDown,true);
 
 }());
